f294535f2c2c0ee429a7ed2e0ea3f7142601e2f2
Vladan Dzulovic <dzulovic.vladan@gmail.com>
2017-12-07 15:26:56 +0100

    Sredjivanje team i portfolio sekcije

--- js/main.js
+++ js/main.js
@@ -1,25 +1,91 @@
-$(document).ready(function(){
-    
-    $(window).scroll(function(){
+$(document).ready(function () {
+
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-        if(scroll > 150){
+        if (scroll > 150) {
             $('header').addClass('py-2');
-        }else{
+        } else {
             $('header').removeClass('py-2');
         }
     });
-    
-    $('.navbar-nav a').click(function(){
+
+    $('.navbar-nav a').click(function () {
         $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 2000);
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
     });
-    
-    
-    
-    if($('.bxslider').length > 0){
+
+
+
+    if ($('.bxslider').length > 0) {
         $('.lead-slider').bxSlider();
     }
-    
-    
+
+    function animation() {
+        var windowHight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            var pozicija = $(this).offset().top;
+            var animacija = $(this).attr('data-animation');
+            if (pozicija < scroll + windowHight - 100) {
+                $(this).addClass(animacija);
+            }
+        });
+
+    }
+
+    animation();
+
+    $(window).scroll(function () {
+        animation();
+    });
+
+
+    if ($('.owl-carousel').length > 0) {
+        $('.team-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                }
+            }
+        });
+        
+        
+        $('.portfolio-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                },
+                1200: {
+                    items: 5
+                }
+            }
+        });
+    }
+
+
 });
